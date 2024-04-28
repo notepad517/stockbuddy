62b0b20d0e3cb5bea30b5c20c1899f9edd8938f2
SahilGrip <sahillather002@gmail.com>
2024-04-28 18:04:03 +0530

    trying fix

--- build_files.sh
+++ build_files.sh
@@ -6,11 +6,11 @@ source ./virtualenv/bin/activate
 # Install dependencies
 python3 -m pip install -r requirements.txt
 
-pip install psycopg2-binary==2.9.9
-
+# # Install psycopg2-binary
+# python3 -m pip install psycopg2-binary==2.9.9
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Deactivate virtual environment
 deactivate
