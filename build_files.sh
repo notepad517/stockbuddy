a043b81edafcf48f8e9b455fd1cba480a521deb7
SahilGrip <sahillather002@gmail.com>
2024-04-28 17:08:35 +0530

    trying build fiax

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Activate virtual environment
+source ./venv/bin/activate
+
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Deactivate virtual environment
+deactivate
