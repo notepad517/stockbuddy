a58e022bd474797dde2a3cb6b1e0167b9580294d
SahilGrip <sahillather002@gmail.com>
2024-04-28 17:45:48 +0530

    fixing up

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,9 @@ source ./virtualenv/bin/activate
 # Install dependencies
 python3 -m pip install -r requirements.txt
 
+pip install psycopg2-binary==2.9.9
+
+
 # Collect static files
 python manage.py collectstatic --noinput
 
