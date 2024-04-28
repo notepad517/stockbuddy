e28fb3cb41c470f54cbc5d034b7c82352c322bb8
SahilGrip <sahillather002@gmail.com>
2024-04-28 16:00:49 +0530

    trying build fix

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
 # build_files.sh
 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --no-input --clear
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
